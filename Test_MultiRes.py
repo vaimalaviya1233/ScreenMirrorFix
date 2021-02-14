57fd5b951ad0849163e0028edf29051c1621fc43
vaimalaviya1233 <vaibhavmalaviya@yahoo.com>
2021-02-13 23:21:55 -0600

    not ready rc

--- Test_MultiRes.py
+++ Test_MultiRes.py
@@ -1,42 +1,58 @@
 #from pynput.mouse import Button, Controller
 #mouse = Controller()
-import pyautogui, time
-a=pyautogui
+import pyautogui
+import time
+a = pyautogui
 pyautogui.FAILSAFE = True
-#simulates wait
+# simulates wait
+
+
 def wait(sec):
     time.sleep(sec)
-#function to reset resolution
+# function to reset resolution
+
+
 def resetsolution():
-    resolution= a.size()
-    a.hotkey('win','d')#minimizes any open windows for sefty protocol
+    resolution = a.size()
+    a.hotkey('win', 'd')  # minimizes any open windows for sefty protocol
     wait(0.5)
-    a.hotkey('win','i')#opens Windows settings with hotkey(shortcut)
+    a.hotkey('win', 'i')  # opens Windows settings with hotkey(shortcut)
     wait(2)
-    a.press('tab')#chooses System option
-    a.press('enter')#presses enter to open system   settings
+    a.press('tab')  # chooses System option
+    a.press('enter')  # presses enter to open system   settings
     wait(1)
-    a.press('tab',8)#goes to resolution dropdown menu
+    # goes to resolution dropdown menu
+    a.press('tab', 8)
     wait(1)
-    a.press('enter',2,3)#choose other resolution than the recommended (3 seconds)
-    a.press('tab')#navigates to keep res settings option when resolution changed
+    # choose other resolution than the recommended (3 seconds)
+    a.press('enter', 2, 3)
+    a.press('tab')  # navigates to keep res settings option when resolution changed
     wait(0.5)
-    a.press('enter')#presses enter to keep resolution
+    a.press('enter')  # presses enter to keep resolution
     resolution = a.size()
     wait(1)
-    a.press('tab',9)#Renavigates to resolution menu
+    a.press('tab', 9)  # Renavigates to resolution menu
     wait(1)
-    a.press('enter',2,3)#gives you only 3 seconds to choose recommended resolution
+    # gives you only 3 seconds to choose recommended resolution
+    a.press('enter', 2, 3)
     wait(0.5)
-    a.press('tab')#navigates to keep res settings
-    a.press('enter')#presses enter to keep resolution which was recommended
-    a.hotkey('win','i')#opens exsting settings window
-    a.hotkey('alt','f4')#closes existing 
-    
+    a.press('tab')  # navigates to keep res settings
+    a.press('enter')  # presses enter to keep resolution which was recommended
+    a.hotkey('win', 'i')  # opens exsting settings window
+    a.hotkey('alt', 'f4')  # closes existing
+
+
 def main():
     resetsolution()
-    status=(a.confirm(text='Did it Worked?',title='Run Again?', buttons=('Worked','Retry')))
+    status = (a.confirm(text='Did it Worked?',
+                        title='Run Again?', buttons=('Worked', 'Retry')))
     if status == 'Retry':
         resetsolution()
+
+
 if __name__ == '__main__':
     main()
+
+res=pyautogui.size
+if (res[0] == 1366) and (res[1] == 768):
+    a.press('tab', 8)
